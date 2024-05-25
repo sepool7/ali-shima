a68fd99b0f9ed15f0b9a71a276297bc9e7c68960
sepool7 <rajaee.sepideh@gmail.com>
2024-05-25 18:18:45 +0200

    first commit

--- src/WeddingWebsite.js
+++ src/WeddingWebsite.js
@@ -4,7 +4,7 @@ import './WeddingWebsite.css'; // Import your CSS file for styling
 function App() {
   const weddingDetails = {
     coupleNames: 'ALI & SHIMA',
-    date: 'Tuesday, AGU 20, 2025',
+    date: 'Tuesday, AUG 20, 2025',
     time: '5:00 PM',
     location: 'https://www.google.com/maps/place/Strada+Scaparoni,+45,+12051+Alba+CN,+Italia/@44.707223,7.905322,17z/data=!3m1!4b1!4m5!3m4!1s0x4724c1f020e2d04f:0x2b0f2f3b6d2d9f13!8m2!3d44.707223!4d7.905322',
     rsvpLink: 'https://docs.google.com/forms/d/1A3yYwhwOvWMU_4BlvU7VdiFzt_Cy_brwpiqQnDBdk6w/edit', // Optional RSVP link
@@ -12,7 +12,7 @@ function App() {
       With hearts full of love, we invite you to celebrate our wedding
       as we begin our journey together as husband and wife.
     `,
-    img: '/pictureReact.jpg', // Path to your main image
+    img: 'pictureReact.jpg', // Path to your main image
   };
 
   return (
@@ -22,9 +22,9 @@ function App() {
       </div>
       <div className="details-container">
         <header>
-          <h1>ALI</h1>
+          <h1>ALI ADIM</h1>
           <h2>and</h2>
-          <h1>SHIMA </h1>
+          <h1>SHIMA GHANDI NEJAD</h1>
         </header>
         <p>{weddingDetails.date} at {weddingDetails.time}</p>
         <p dangerouslySetInnerHTML={{ __html: weddingDetails.description }} />
